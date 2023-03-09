8f010f5bb033208915689ec81f8af6ac38e06828
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-03-08 16:05:49 -0800

    finished search :>

--- social-media-react-dev/src/Components/Search/search.jsx
+++ social-media-react-dev/src/Components/Search/search.jsx
@@ -71,8 +71,32 @@ function Search() {
               <span>Tag: </span>
               <select value={searchTag} onChange={handleSearchTagChange}>
                 <option value={0}>All</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
+                <option value={1}>Aggravating</option>
+                <option value={2}>Amusing</option>
+                <option value={3}>Anxious</option>
+                <option value={4}>Bad</option>
+                <option value={5}>Boring</option>
+                <option value={6}>Challenging</option>
+                <option value={7}>Childish</option>
+                <option value={8}>Confusing</option>
+                <option value={9}>Discouraging</option>
+                <option value={10}>Disliked</option>
+                <option value={11}>Engaging</option>
+                <option value={12}>Friendly</option>
+                <option value={13}>Frustrating</option>
+                <option value={14}>Fulfilling</option>
+                <option value={15}>GLHF</option>
+                <option value={16}>Good</option>
+                <option value={17}>Inconsiderate</option>
+                <option value={18}>Infuriating</option>
+                <option value={19}>Musical</option>
+                <option value={20}>Nerd</option>
+                <option value={21}>Offputting</option>
+                <option value={22}>Rage-inducing</option>
+                <option value={23}>Respected</option>
+                <option value={24}>Respectful</option>
+                <option value={25}>Stressful</option>
+                <option value={26}>Understanding</option>
               </select>
             </div>
             <div className="search-emp-department">
@@ -82,8 +106,18 @@ function Search() {
                 onChange={handleSearchDepartmentChange}
               >
                 <option value={0}>All</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
+                <option value={1}>Accounting Team</option>
+                <option value={2}>Center of Excellence</option>
+                <option value={3}>HR Team</option>
+                <option value={4}>IT Support</option>
+                <option value={5}>Legal Team</option>
+                <option value={6}>Management</option>
+                <option value={7}>Marketing Team</option>
+                <option value={8}>PDP Team</option>
+                <option value={9}>QC Team</option>
+                <option value={10}>Recruitment Department</option>
+                <option value={11}>Sales Department</option>
+                <option value={12}>Training Department</option>
               </select>
             </div>
           </div>
